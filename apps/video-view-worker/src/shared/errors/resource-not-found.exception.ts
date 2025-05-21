9330e620b1e250a38040b198ebbab2e52e244a72
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 18:05:10 -0300

    feat: add ResourceNotFoundException class for improved error handling

---
+++ apps/video-view-worker/src/shared/errors/resource-not-found.exception.ts
@@ -0,0 +1,13 @@
+import { BaseException } from "./base.exception"
+
+const RESOURCE_NOT_FOUND_ERROR_CODE = 404
+
+export class ResourceNotFoundException extends BaseException {
+  public readonly code: number
+
+  constructor(resource: string = 'Resource') {
+    super(`${resource} not found.`)
+    this.name = 'ResourceNotFoundException'
+    this.code = RESOURCE_NOT_FOUND_ERROR_CODE
+  }
+}
