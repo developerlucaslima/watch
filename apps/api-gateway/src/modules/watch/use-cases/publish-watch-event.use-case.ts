8a02eaa89273535e7864931dd3e468cdaea44d5a
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 10:10:38 -0300

    refactor: remove unused publishWatchEvent and related WatchEvent use case

--- apps/api-gateway/src/modules/watch/use-cases/publish-watch-event.use-case.ts
+++
@@ -1,8 +0,0 @@
-import { publishWatchEvent } from '@services/sqs-publisher.service'
-import { WatchEvent } from '../dtos/watch-event.dto'
-
-export class PublishWatchEventUseCase {
-  async execute(data: WatchEvent): Promise<void> {
-    await publishWatchEvent(data)
-  }
-}
