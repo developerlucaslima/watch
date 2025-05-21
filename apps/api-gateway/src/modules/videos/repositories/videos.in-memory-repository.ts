0b90424079d25d49c84cdb773c558740d90d8cdf
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 09:36:12 -0300

    feat: implement in-memory repository for video management

---
+++ apps/api-gateway/src/modules/videos/repositories/videos.in-memory-repository.ts
@@ -0,0 +1,26 @@
+import { randomUUID } from 'crypto'
+import type { Video, VideoCreateParams } from '@shared/types/video'
+import type { IVideosRepository } from './videos.interface-repository'
+
+export class VideosInMemoryRepository implements IVideosRepository {
+  private videosById = new Map<string, Video>()
+
+  async create(data: VideoCreateParams): Promise<Video> {
+    const newVideo: Video = {
+      id: randomUUID(),
+      createdAt: new Date(),
+      ...data,
+    }
+
+    this.videosById.set(newVideo.id, newVideo)
+    return newVideo
+  }
+
+  async findAll(): Promise<Video[]> {
+    return Array.from(this.videosById.values())
+  }
+
+  async findById(id: string): Promise<Video | null> {
+    return this.videosById.get(id) ?? null
+  }
+}
