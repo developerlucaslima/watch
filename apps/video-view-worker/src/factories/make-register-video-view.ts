7ebd70c9e7121d76e0a01c8d5ed822bad0369feb
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 19:06:01 -0300

    feat: create factory function for RegisterVideoView use case

---
+++ apps/video-view-worker/src/factories/make-register-video-view.ts
@@ -0,0 +1,16 @@
+import { UsersPrismaRepository } from "@repositories/user/users.prisma-repository"
+import { VideoViewsPrismaRepository } from "@repositories/video-view/video-views.prisma-repository"
+import { VideosPrismaRepository } from "@repositories/video/videos.prisma-repository"
+import { RegisterVideoViewUseCase } from "@use-cases/register-video-view.use-case"
+
+export function makeRegisterVideoView() {
+  const videoViewsPrismaRepository = new VideoViewsPrismaRepository()
+  const usersPrismaRepository = new UsersPrismaRepository()
+  const videosPrismaRepository = new VideosPrismaRepository()
+
+  return new RegisterVideoViewUseCase(
+    videoViewsPrismaRepository,
+    usersPrismaRepository,
+    videosPrismaRepository,
+  )
+}
