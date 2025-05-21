74a9c90adcbd84e29643d23e6110cf4539d13749
Lucas Lima <101366755+developerlucaslima@users.noreply.github.com>
2025-05-21 05:38:44 -0300

    Delete node_modules directory

--- node_modules/src/src/routes/watch.ts
+++
@@ -1,6 +0,0 @@
-import { publishWatchEventController } from '@watch/controllers/publish-watch-event.controller'
-import { FastifyInstance } from 'fastify'
-
-export async function watchRoutes(app: FastifyInstance) {
-  await publishWatchEventController(app)
-}
