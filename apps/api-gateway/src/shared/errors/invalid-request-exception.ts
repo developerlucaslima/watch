fc0f36d777ea265767695fd0115cb0f0ce519bb7
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 17:42:36 -0300

    fix: update InvalidRequestException constructor to enforce message parameter

--- apps/api-gateway/src/shared/errors/invalid-request-exception.ts
+++ apps/api-gateway/src/shared/errors/invalid-request-exception.ts
@@ -5,8 +5,8 @@ const BAD_REQUEST_ERROR_CODE = 400
 export class InvalidRequestException extends BaseException {
   public readonly code: number
 
-  constructor(message = 'Invalid request.') {
-    super(message)
+  constructor(message: string) {
+    super(`Invalid request: ${message}`)
     this.name = 'InvalidRequestException'
     this.code = BAD_REQUEST_ERROR_CODE
   }
