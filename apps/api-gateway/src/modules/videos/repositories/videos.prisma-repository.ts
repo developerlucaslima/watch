270cee16610905999cb0a1da5230633dad9799fa
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 09:36:23 -0300

    feat: implement VideosPrismaRepository for video data access

---
+++ apps/api-gateway/src/modules/videos/repositories/videos.prisma-repository.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@database/prisma"
+import type { IVideosRepository } from "./videos.interface-repository"
+
+export class VideosPrismaRepository implements IVideosRepository {
+  async findAll() {
+    return prisma.video.findMany()
+  }
+
+  async findById(id: string) {
+    return prisma.video.findUnique({ where: { id } })
+  }
+}
