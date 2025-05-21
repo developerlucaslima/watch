b907fde9fc47bb94fcc0521abdf6768f5b751041
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 07:30:33 -0300

    feat: add name field to User type and update UserWithoutPassword type

--- apps/api-gateway/src/shared/types/user.ts
+++ apps/api-gateway/src/shared/types/user.ts
@@ -1,8 +1,11 @@
 export type User = {
   id: string;
+  name: string;
   email: string;
   passwordHash: string;
   createdAt: Date;
 }
+
+export type UserWithoutPassword = Omit<User, 'passwordHash'>
 
 export type UserCreateParams = Omit<User, 'id' | 'createdAt'>
