6f3bfa525d0fb414ff14bfe54b0ded10ccf2614b
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 07:47:26 -0300

    feat: add mapAuthenticatedUserResponse function to transform UserWithoutPassword data

---
+++ apps/api-gateway/src/modules/auth/dtos/user.ts
@@ -0,0 +1,12 @@
+import type { UserWithoutPassword } from "@shared/types/user";
+
+export function mapAuthenticatedUserResponse(
+  user: UserWithoutPassword,
+) {
+  return {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    createdAt: user.createdAt,
+  }
+}
