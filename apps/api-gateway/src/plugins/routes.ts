db124259eb19c8e373cd84c16f1e7e7c69b182fb
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 10:13:34 -0300

    feat: register watchRoutes for handling video watch requests

--- apps/api-gateway/src/plugins/routes.ts
+++ apps/api-gateway/src/plugins/routes.ts
@@ -1,8 +1,10 @@
 import { authRoutes } from '@modules/auth/routes/auth.route'
 import { videosRoutes } from '@modules/videos/routes/videos.route'
+import { watchRoutes } from '@modules/watch/routes/watch.route'
 import type { FastifyInstance } from 'fastify'
 
 export function routesRegister(app: FastifyInstance) {
   app.register(authRoutes, { prefix: '/auth' })
   app.register(videosRoutes, { prefix: '/videos' })
+  app.register(watchRoutes, { prefix: '/watch' })
 }
