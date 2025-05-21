4150a426fdc9dffe7490115e7a02788b3293760b
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 06:55:30 -0300

    fix: update import path for globalErrorHandler to use shared/errors

--- apps/api-gateway/src/plugins/error.ts
+++ apps/api-gateway/src/plugins/error.ts
@@ -1,6 +1,6 @@
 import type { FastifyInstance } from 'fastify'
 
-import { globalErrorHandler } from '@errors/global-error-handler'
+import { globalErrorHandler } from 'shared/errors/global-error-handler'
 
 export function routesRegister(app: FastifyInstance) {
   app.setErrorHandler(globalErrorHandler)
