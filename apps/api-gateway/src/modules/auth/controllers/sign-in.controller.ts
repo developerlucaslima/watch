f1309e5c83fd10ce28f9276ffaca026c849e1dc9
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 07:47:36 -0300

    feat: implement sign-in controller for user authentication

---
+++ apps/api-gateway/src/modules/auth/controllers/sign-in.controller.ts
@@ -0,0 +1,41 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeSignIn } from '../factories/make-sign-in'
+import { ACCESS_TOKEN_EXPIRATION_SECONDS, REFRESH_TOKEN_EXPIRATION_SECONDS } from 'middlewares/jwt/jwt-config'
+import { setAuthCookies } from 'middlewares/jwt/set-auth-cookies'
+import { mapAuthenticatedUserResponse } from '../dtos/user'
+
+const signInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+export async function signInController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const { email, password } = signInSchema.parse(request.body)
+  const signInUseCase = makeSignIn()
+  const { user } = await signInUseCase.execute({
+    email,
+    password,
+  })
+
+  const accessToken = await reply.jwtSign({
+    sign: {
+      sub: user.id,
+      expiresIn: `${ACCESS_TOKEN_EXPIRATION_SECONDS}s`,
+    },
+  })
+
+  const refreshToken = await reply.jwtSign({
+    sign: {
+      sub: user.id,
+      expiresIn: `${REFRESH_TOKEN_EXPIRATION_SECONDS}s`,
+    },
+  })
+
+  setAuthCookies(reply, accessToken, refreshToken)
+
+  return reply.status(200).send(mapAuthenticatedUserResponse(user))
+}
