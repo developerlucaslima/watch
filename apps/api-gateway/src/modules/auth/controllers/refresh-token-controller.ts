16ed90212764e6d7dd5a9c09f051e74156ebf130
developerlucaslima <developerlucaslima@gmail.com>
2025-05-21 07:51:49 -0300

    feat: implement refresh token controller for JWT token renewal functionality

---
+++ apps/api-gateway/src/modules/auth/controllers/refresh-token-controller.ts
@@ -0,0 +1,34 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { ACCESS_TOKEN_EXPIRATION_SECONDS, REFRESH_TOKEN_EXPIRATION_SECONDS } from 'middlewares/jwt/jwt-config'
+import { setAuthCookies } from 'middlewares/jwt/set-auth-cookies'
+
+export async function refreshTokenController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  await request.jwtVerify({ onlyCookie: true })
+
+  const newAccessToken = await reply.jwtSign(
+    {
+      sign: {
+        sub: request.user,
+        expiresIn: `${ACCESS_TOKEN_EXPIRATION_SECONDS}s`,
+      },
+    },
+  )
+
+  const newRefreshToken = await reply.jwtSign(
+    {
+      sign: {
+        sub: request.user,
+        expiresIn: `${REFRESH_TOKEN_EXPIRATION_SECONDS}s`,
+      },
+    },
+  )
+
+  setAuthCookies(reply, newAccessToken, newRefreshToken)
+
+  return reply.status(200).send({
+    message: 'Token refreshed successfully.',
+  })
+}
