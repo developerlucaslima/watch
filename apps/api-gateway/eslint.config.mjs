7dcee986f92b843e5aade211ef4a2f35b36862ce
developerlucaslima <developerlucaslima@gmail.com>
2025-05-22 16:52:57 -0300

    chore: update ESLint and Prettier configurations
    - Added eslint-config-prettier to disable conflicting rules
    - Added eslint-plugin-prettier to integrate Prettier with ESLint
    - Updated Prettier to version 3.5.3

--- apps/api-gateway/eslint.config.mjs
+++ apps/api-gateway/eslint.config.mjs
@@ -2,6 +2,7 @@ import tsPlugin from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import importPlugin from 'eslint-plugin-import'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default [
   {
@@ -16,8 +17,9 @@ export default [
     },
     plugins: {
       '@typescript-eslint': tsPlugin,
-      'import': importPlugin,
+      import: importPlugin,
       'simple-import-sort': simpleImportSort,
+      prettier: prettierPlugin,
     },
     settings: {
       'import/resolver': {
@@ -27,10 +29,35 @@ export default [
       },
     },
     rules: {
+      // TypeScript
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+      // Imports
+      'import/no-unresolved': 'error',
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-      'import/no-unresolved': 'error',
+
+      // Código limpo
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'eol-last': ['error', 'always'],
+      'indent': ['error', 2, { SwitchCase: 1 }],
+      'object-curly-spacing': ['error', 'always'],
+      'max-len': ['warn', { code: 120, tabWidth: 2, ignoreUrls: true }],
+
+      // Prettier integrado
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: false,
+          endOfLine: 'auto'
+        },
+      ],
     },
   },
   {
