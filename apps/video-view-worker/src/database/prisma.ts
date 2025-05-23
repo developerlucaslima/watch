95c3859bb350e545274605c45ea5a9774ca55ea4
developerlucaslima <developerlucaslima@gmail.com>
2025-05-23 20:50:15 -0300

    refactor: implement global Prisma client instance for improved performance

--- apps/video-view-worker/src/database/prisma.ts
+++ apps/video-view-worker/src/database/prisma.ts
@@ -1,10 +1,18 @@
 import { env } from '@env'
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: env.DATABASE_URL,
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: env.DATABASE_URL,
+      },
     },
-  },
-})
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
