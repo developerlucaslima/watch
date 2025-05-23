18ab2d9249b9160aabd1db433697afdbe1b745d7
developerlucaslima <developerlucaslima@gmail.com>
2025-05-23 14:33:41 -0300

    fix: handle OPTIONS requests in JWT verification middleware

--- apps/api-gateway/src/plugins/error.ts
+++
@@ -1,6 +0,0 @@
-import { globalErrorHandler } from '@shared/errors/global-error.handler'
-import type { FastifyInstance } from 'fastify'
-
-export function routesRegister(app: FastifyInstance) {
-  app.setErrorHandler(globalErrorHandler)
-}
